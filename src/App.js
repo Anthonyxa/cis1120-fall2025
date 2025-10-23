934a793972a59a6fdfda74fab30edf9ec5c91d62
Anthonyxa <albedokun3@gmail.com>
2025-10-23 11:29:34 -0400

    Implement routing with BrowserRouter and Routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import "./styles.css";
-import Resume from "./pages/Resume";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
 
 export default function App() {
-  return <Resume />;
+  return (
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
