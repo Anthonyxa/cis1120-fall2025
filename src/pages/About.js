eed69079e48e158efd8209a3ae1a75c124ca921f
Anthonyxa <albedokun3@gmail.com>
2025-10-23 11:27:50 -0400

    Add About page with organization details

---
+++ src/pages/About.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <>
+      <h1>About Our Organization</h1>
+      <p>We focus on outreach, education, and volunteer work to improve lives.</p>
+    </>
+  );
+}
