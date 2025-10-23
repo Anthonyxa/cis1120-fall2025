de9d322371b4a7fdfd12558d94d988de2dce7676
Anthonyxa <albedokun3@gmail.com>
2025-10-23 11:27:27 -0400

    Create Home component for non-profit welcome page

---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <>
+      <h1>Welcome to Our Non-Profit</h1>
+      <p>We’re dedicated to making a difference in our community.</p>
+    </>
+  );
+}
